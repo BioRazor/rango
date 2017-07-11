1d0f702ffb448dacd0a64c76b24387be4df4ce4d
d3aCanaima <biorazor.a@gmail.com>
2017-07-10 21:10:01 -0400

    Started chapter 10. Finished previus points.

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
@@ -35,7 +38,7 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context_dict)
 
-
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -49,6 +52,7 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form':form})
 
+@login_required
 def add_page(request, category_name_slug):
 
     try:
@@ -93,8 +97,34 @@ def register(request):
                  profile.picture = request.FILES('picture')
             profile.save()
             registered = True
+
+            login(request, user=authenticate(username=user.username, password=user.password))
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
     return render(request, 'rango/register.html', {'user_form' : user_form, 'profile_form' : UserProfileForm, 'registered' : registered}
 )
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username = username, password = password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse('Your Rango account is disabled')
+        else:
+            print('Invalid login details: {0}, {1}'.format(username, password))
+            return HttpResponse('Invalid Login details supplied.')
+    else:
+        return render(request, 'rango/user_login.html', {})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -130,3 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'index'
+
+LOGIN_URL = 'login'
